ada8d1cbaaf64094ba9f79a81d5753550bd1d392
capboyaryan <capboyaryan@gmail.com>
2022-06-25 13:34:59 +0530

    added function prototypes in club.h

--- include/Club.h
+++ include/Club.h
@@ -24,6 +24,11 @@ class Club
     void display_co_convener() const;
     void display_social_media_id() const;
     void display_description() const;
+    std::string get_name();
+    std::string get_convener();
+    std::string get_co_convener();
+    std::string get_social_media_id();
+    std::string get_description();
     Club(std::string name,std::string convener,std::string co_convener,std::string social_media_id,std::string description);
     friend void edit_club(std::vector<Club>& club_info ,std::fstream& club_file );
 };
