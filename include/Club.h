642d7511b03cd9cbb009a1b4feb949f2a77c31b9
Hargun Kaur <hargunkaur625@gmail.com>
2022-07-10 08:51:43 +0530

    ..

--- include/Club.h
+++ include/Club.h
@@ -15,22 +15,18 @@ class Club
     std::string description;
     public:
     Club(std::fstream &);
-    std::string get_name() const
-    {
-        return name;
-    }
     void display_name() const;
     void display_convener() const;
     void display_co_convener() const;
     void display_social_media_id() const;
     void display_description() const;
+    Club(std::string name,std::string convener,std::string co_convener,std::string social_media_id,std::string description);
+    friend void edit_club(std::vector<Club>& club_info ,std::fstream& club_file );
     std::string get_name();
     std::string get_convener();
     std::string get_co_convener();
     std::string get_social_media_id();
     std::string get_description();
-    Club(std::string name,std::string convener,std::string co_convener,std::string social_media_id,std::string description);
-    friend void edit_club(std::vector<Club>& club_info ,std::fstream& club_file );
 };
 
 #endif
