522c5c70b3f23ead13611dbcf86478eed0ce5f6f
Hargun Kaur <hargunkaur625@gmail.com>
2022-12-28 20:51:37 +0530

    colours added

--- GUI.py
+++ GUI.py
@@ -9,12 +9,25 @@ heading= Label(root,text="Clubs and Event Management System",font="timesnewroman
 heading.pack()
 btn_frame= Frame(root)
 btn_frame.pack()
-clubs_btn= Button(btn_frame,text="Clubs",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12)
+clubs_btn= Button(btn_frame,text="Clubs",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#999999')
 clubs_btn.grid(row=0, column=0,padx=4,pady=4)
-Events_btn= Button(btn_frame,text="Events",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12)
+Events_btn= Button(btn_frame,text="Events",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#999999')
 Events_btn.grid(row=0, column=1)
-admin_btn= Button(btn_frame,text= "Admin",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12)
+admin_btn= Button(btn_frame,text= "Admin",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#999999')
 admin_btn.grid(row=1, column=0,padx=4)
-exit_btn= Button(btn_frame,text= "Exit",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12)
+exit_btn= Button(btn_frame,text= "Exit",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#999999',command=root.destroy)
 exit_btn.grid(row=1, column=1)
+def on_enter(e):
+   e.widget['background'] = '#666666'
+def on_leave(e):
+   e.widget['background'] = '#ffffff'
+
+clubs_btn.bind('<Enter>', on_enter)
+clubs_btn.bind('<Leave>', on_leave)
+Events_btn.bind('<Enter>', on_enter)
+Events_btn.bind('<Leave>', on_leave)
+admin_btn.bind('<Enter>', on_enter)
+admin_btn.bind('<Leave>', on_leave)
+exit_btn.bind('<Enter>', on_enter)
+exit_btn.bind('<Leave>', on_leave)
 root.mainloop()
