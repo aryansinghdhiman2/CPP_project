43057e28ffb5150e12569597db21553b652bf089
Hargun Kaur <hargunkaur625@gmail.com>
2022-12-29 18:01:52 +0530

    .

--- GUI.py
+++ GUI.py
@@ -3,26 +3,29 @@ root= Tk()
 def club_window():
    club_win= Toplevel(root)
    root.withdraw()
+   bg1=PhotoImage(file="newbg.png")
+   l1=Label(club_win,image=bg1)
+   l1.place(x=0,y=0,relheight=1,relwidth=1)
    club_win.geometry("1000x663")
    club_win.resizable(False,False)
    club_win.title("Clubs")
    def func():
       club_win.destroy()
       root.deiconify()
-   main_frame= Frame(club_win,bg="#ffffff",height=300,width=700,borderwidth=1,relief=RIDGE)
+   main_frame= Frame(club_win,bg="pale turquoise",height=300,width=700,borderwidth=1,relief=RIDGE)
    main_frame.pack(pady=100)
    main_frame.pack_propagate(0)
-   heading= Label(main_frame,text="Club Menu",font="timesnewroman 30 ", padx=15,pady=25,bg="#ffffff")
+   heading= Label(main_frame,text="Club Menu",font="timesnewroman 30 ", padx=15,pady=25,bg="pale turquoise")
    heading.pack()
-   btn_frame= Frame(main_frame,bg="#ffffff")
+   btn_frame= Frame(main_frame,bg="pale turquoise")
    btn_frame.pack(pady=20)
-   info_btn= Button(btn_frame,text="Information",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff')
+   info_btn= Button(btn_frame,text="Information",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise')
    info_btn.grid(row=0, column=0,padx=4,pady=4)
-   recr_btn= Button(btn_frame,text="Recruitment",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff')
+   recr_btn= Button(btn_frame,text="Recruitment",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise')
    recr_btn.grid(row=0, column=1)
-   meet_btn= Button(btn_frame,text= "Meeting",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff')
+   meet_btn= Button(btn_frame,text= "Meeting",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise')
    meet_btn.grid(row=1, column=0,padx=4)
-   return_btn= Button(btn_frame,text= "Return",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff',command=func)
+   return_btn= Button(btn_frame,text= "Return",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise',command=func)
    return_btn.grid(row=1, column=1)
    info_btn.bind('<Enter>', on_enter)
    info_btn.bind('<Leave>', on_leave)
@@ -36,7 +39,7 @@ def club_window():
 def on_enter(e):
    e.widget['background'] = '#999999'
 def on_leave(e):
-   e.widget['background'] = '#ffffff'
+   e.widget['background'] = 'pale turquoise'
 
 
 mybg= PhotoImage(file="newbg.png")
@@ -55,13 +58,13 @@ heading= Label(main_frame,text="Clubs and Event Management System",font="timesne
 heading.pack()
 btn_frame= Frame(main_frame,bg="pale turquoise")
 btn_frame.pack()
-clubs_btn= Button(btn_frame,text="Clubs",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff',command=club_window)
+clubs_btn= Button(btn_frame,text="Clubs",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise',command=club_window)
 clubs_btn.grid(row=0, column=0,padx=4,pady=4)
-Events_btn= Button(btn_frame,text="Events",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff')
+Events_btn= Button(btn_frame,text="Events",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise')
 Events_btn.grid(row=0, column=1)
-admin_btn= Button(btn_frame,text= "Admin",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff')
+admin_btn= Button(btn_frame,text= "Admin",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise')
 admin_btn.grid(row=1, column=0,padx=4)
-exit_btn= Button(btn_frame,text= "Exit",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#666666',bg='#ffffff',command=root.destroy)
+exit_btn= Button(btn_frame,text= "Exit",font="timesnewroman 20",relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#ffffff',bg='pale turquoise',command=root.destroy)
 exit_btn.grid(row=1, column=1)
 
 
