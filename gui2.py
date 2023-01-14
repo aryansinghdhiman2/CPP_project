fff23ca7cf9d2215b95815b49a84cf44e8dc06a6
Hargun Kaur <hargunkaur625@gmail.com>
2023-01-14 21:57:53 +0530

    .

--- gui2.py
+++ gui2.py
@@ -18,6 +18,8 @@ myFont= Font(family="Calibri",size=30,weight="bold")
 myFont1= Font(family="Calibri",size=18,weight="bold")
 myFont2= Font(family="Calibri",size=15,weight="bold")
 myFont3= Font(family="Calibri",size=13)
+myFont4= Font(family="Century Gothic",size=16,weight="bold")
+myFont5= Font(family="Century Gothic",size=13,weight="bold")
 root.geometry("1280x720")
 root.title("Clubs and Event Management System")
 # screen_width = root.winfo_screenwidth()
@@ -30,53 +32,30 @@ def club_window():
     club_win.geometry("1280x720")
     main_menu= Label(club_win,font=myFont,bg="#023047",text="Club Menu",fg="white")
     main_menu.place(x=0,y=0,relwidth=1,height=45)
-    # manage_frame= Frame(club_win,bd=4,bg="yellow",relief=RIDGE)
-    # manage_frame.place(x=20,y=100,width=450,height=460)
     def club_info():
-        manage_frame= Frame(club_win,bd=4,bg="#8ecae6",relief=RIDGE)
-        manage_frame.place(x=0,y=100,width=480,height=540)
         display_frame= Frame(club_win,bd=4,bg="#8ecae6",relief=RIDGE)
-        display_frame.place(x=500,y=100,height=540,width=760)
+        display_frame.place(x=25,y=115,height=540,width=1215)
 
-        l1=Label(manage_frame,text="Club name",font=myFont1,bg="#8ecae6")
-        l1.grid(row=0,column=0,pady=30,padx=20)
-
-        name_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        name_entry.grid(row=0,column=1)
-
-        l2=Label(manage_frame,text="Convener",font=myFont1,bg="#8ecae6")
-        l2.grid(row=1,column=0,pady=30,padx=20)
-
-        con_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        con_entry.grid(row=1,column=1)
-
-        l3=Label(manage_frame,text="Social Media",font=myFont1,bg="#8ecae6")
-        l3.grid(row=2,column=0,pady=30,padx=20)
-
-        sc_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        sc_entry.grid(row=2,column=1)
-
-        l4=Label(manage_frame,text="Description",font=myFont1,bg="#8ecae6")
-        l4.grid(row=3,column=0,pady=40,padx=20)
-
-        des_entry=Text(manage_frame,font=myFont3,width=20,height=4,relief=SUNKEN,bd=3)
-        des_entry.grid(row=3,column=1)
-
-        search_lbl= Label(display_frame,text="Search by",bg="#8ecae6",font=myFont1)
-        search_lbl.grid(row=0,column=0,sticky="w",pady=10,padx=20)
+        
+        srch_frame= Frame(display_frame,bd=2,bg="#219ebc",relief=FLAT)
+        srch_frame.place(x=290,y=10)
+        search_lbl= Label(srch_frame,text="Search by",bg="#219ebc",font=myFont4,fg="white")
+        search_lbl.grid(row=0,column=0,sticky="w",pady=5,padx=10)
 
         srch_var=StringVar()
         srch_entry_var=StringVar()
-
-        combo_search= ttk.Combobox(display_frame,width=10,font=myFont2,state="readonly",textvariable=srch_var)
+        # style= ttk.Style()
+        # style.theme_use("default")
+        combo_search= ttk.Combobox(srch_frame,width=10,font=myFont2,state="readonly",textvariable=srch_var)
         combo_search['values']=("Club name","social media")
         combo_search.grid(row=0,column=1,pady=10,padx=10)
-        srch_entry=Entry(display_frame,font=myFont3,width=20,relief=SUNKEN,bd=3,textvariable=srch_entry_var)
+        srch_entry=Entry(srch_frame,font=myFont3,width=20,relief=SUNKEN,bd=3,textvariable=srch_entry_var)
         srch_entry.grid(row=0,column=2)
         
         display2_frame= Frame(display_frame,bd=4,bg="#8ecae6",relief=RIDGE)
-        display2_frame.place(x=10,y=65,height=450,width=735)
+        display2_frame.place(x=10,y=70,height=450,width=1185)
         clb_info_tb= ttk.Treeview(display2_frame,columns=("Club_Name","Convener","Social Media","Description"))
+        # clb_info_tb.tag_configure("evenrow",background="lightblue")
         clb_info_tb.heading("Club_Name",text="Club Name")
         clb_info_tb.heading("Convener",text="Convener")
         clb_info_tb.heading("Social Media",text="Social Media")
@@ -87,43 +66,19 @@ def club_window():
         clb_info_tb.column("Social Media",width=150)
         clb_info_tb.column("Description",width=300)
         clb_info_tb.pack(fill=BOTH,expand=1)
-        srch_btn=Button(display_frame,text="Search",font=myFont3,bd=3,relief=GROOVE,command=lambda:search_club(connection,clb_info_tb,srch_var,srch_entry_var)).grid(row=0,column=3,padx=10)
+        srch_btn=Button(srch_frame,text="Search",font=myFont5,bd=3,bg="#219ebc",fg="white",relief=RAISED,command=lambda:search_club(connection,clb_info_tb,srch_var,srch_entry_var)).grid(row=0,column=3,padx=10)
         
-        showall= Button(display_frame,text="Show All",font=myFont3,bd=3,relief=GROOVE,command=lambda:fetch_club_info(connection,clb_info_tb)).grid(row=0,column=4,padx=10)
+        showall= Button(srch_frame,text="Show All",font=myFont5,bd=3,bg="#219ebc",fg="white",relief=RAISED,command=lambda:fetch_club_info(connection,clb_info_tb)).grid(row=0,column=4,padx=10)
 
     def club_meeting():
-        manage_frame= Frame(club_win,bd=4,bg="#8ecae6",relief=RIDGE)
-        manage_frame.place(x=0,y=100,width=480,height=540)
+        
         display_frame= Frame(club_win,bd=4,bg="#8ecae6",relief=RIDGE)
         display_frame.place(x=500,y=100,height=540,width=760)
 
         srch_var=StringVar()
         srch_entry_var=StringVar()
 
-        l1=Label(manage_frame,text="Club Name",font=myFont1,bg="#8ecae6")
-        l1.grid(row=0,column=0,pady=30,padx=20)
-
-        name_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        name_entry.grid(row=0,column=1)
-
-        l2=Label(manage_frame,text="Meeting Date",font=myFont1,bg="#8ecae6")
-        l2.grid(row=1,column=0,pady=30,padx=20)
-
-        con_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        con_entry.grid(row=1,column=1)
-
-        l3=Label(manage_frame,text="Venue",font=myFont1,bg="#8ecae6")
-        l3.grid(row=2,column=0,pady=30,padx=20)
-
-        sc_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        sc_entry.grid(row=2,column=1)
-
-        l4=Label(manage_frame,text="Description",font=myFont1,bg="#8ecae6")
-        l4.grid(row=3,column=0,pady=40,padx=20)
-
-        des_entry=Text(manage_frame,font=myFont3,width=20,height=4,relief=SUNKEN,bd=3)
-        des_entry.grid(row=3,column=1)
-
+        
         search_lbl= Label(display_frame,text="Search by",bg="#8ecae6",font=myFont1)
         search_lbl.grid(row=0,column=0,sticky="w",pady=10,padx=20)
         combo_search= ttk.Combobox(display_frame,width=10,font=myFont2,state="readonly",textvariable=srch_var)
@@ -150,31 +105,31 @@ def club_window():
         srch_btn=Button(display_frame,text="Search",font=myFont3,bd=3,relief=GROOVE,command=lambda:search_meeting(connection,clb_meet_tb,srch_var,srch_entry_var)).grid(row=0,column=3,padx=10)
         showall= Button(display_frame,text="Show All",font=myFont3,bd=3,relief=GROOVE,command=lambda:fetch_club_meeting(connection,clb_meet_tb)).grid(row=0,column=4,padx=10)
     def club_recr():
-        manage_frame= Frame(club_win,bd=4,bg="#8ecae6",relief=RIDGE)
-        manage_frame.place(x=0,y=100,width=480,height=540)
+        # manage_frame= Frame(club_win,bd=4,bg="#8ecae6",relief=RIDGE)
+        # manage_frame.place(x=0,y=100,width=480,height=540)
         display_frame= Frame(club_win,bd=4,bg="#8ecae6",relief=RIDGE)
         display_frame.place(x=500,y=100,height=540,width=760)
 
         srch_var=StringVar()
         srch_entry_var=StringVar()
 
-        l1=Label(manage_frame,text="Club name",font=myFont1,bg="#8ecae6")
-        l1.grid(row=0,column=0,pady=30,padx=20)
+        # l1=Label(manage_frame,text="Club name",font=myFont1,bg="#8ecae6")
+        # l1.grid(row=0,column=0,pady=30,padx=20)
 
-        name_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        name_entry.grid(row=0,column=1)
+        # name_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
+        # name_entry.grid(row=0,column=1)
 
-        l2=Label(manage_frame,text="Recruitment Date",font=myFont1,bg="#8ecae6")
-        l2.grid(row=1,column=0,pady=30,padx=20)
+        # l2=Label(manage_frame,text="Recruitment Date",font=myFont1,bg="#8ecae6")
+        # l2.grid(row=1,column=0,pady=30,padx=20)
 
-        con_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        con_entry.grid(row=1,column=1)
+        # con_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
+        # con_entry.grid(row=1,column=1)
 
-        l3=Label(manage_frame,text="Venue",font=myFont1,bg="#8ecae6")
-        l3.grid(row=2,column=0,pady=30,padx=20)
+        # l3=Label(manage_frame,text="Venue",font=myFont1,bg="#8ecae6")
+        # l3.grid(row=2,column=0,pady=30,padx=20)
 
-        con_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
-        con_entry.grid(row=2,column=1)
+        # con_entry=Entry(manage_frame,font=myFont3,width=20,relief=SUNKEN,bd=3)
+        # con_entry.grid(row=2,column=1)
 
         search_lbl= Label(display_frame,text="Search by",bg="#8ecae6",font=myFont1)
         search_lbl.grid(row=0,column=0,sticky="w",pady=10,padx=20)
@@ -215,9 +170,9 @@ def club_window():
 
 header= Label(root,font=myFont,bg='#14213d',text="Clubs and Event Management System",fg="white")
 header.place(x=0,y=0,relwidth=1,height=60)
-main_menu= Label(root,font=myFont,bg="#fca311",text="Main Menu",fg="white")
+main_menu= Label(root,font=myFont,bg="#ffb703",text="Main Menu",fg="white")
 main_menu.place(x=0,y=60,relwidth=1,height=45)
-f1=Label(root,bg="#e5e5e5")
+f1=Label(root,bg="#cad2c5")
 f1.place(x=0,y=100,relwidth=1,height=350)
 f2=Label(root,bg="#cad2c5")
 f2.place(x=0,y=659,relwidth=1,height=27)
@@ -233,13 +188,13 @@ c3.pack_propagate(0)
 c4= Frame(root,height=250,width=225,bg="#023047")
 c4.place(x=1000,y=250)
 c4.pack_propagate(0)
-clubs_btn= Button(c1,text="Clubs",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#fca311',command=club_window)
+clubs_btn= Button(c1,text="Clubs",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#ffb703',command=club_window)
 clubs_btn.pack(pady=15)
-Events_btn= Button(c2,text="Events",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#fca311',command=lambda:event_window(root,connection))
+Events_btn= Button(c2,text="Events",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#ffb703',command=lambda:event_window(root,connection))
 Events_btn.pack(pady=15)
-admin_btn= Button(c3,text= "Admin",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#fca311',command=lambda:login(root,connection))
+admin_btn= Button(c3,text= "Admin",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#ffb703',command=lambda:login(root,connection))
 admin_btn.pack(pady=15)
-exit_btn= Button(c4,text= "Exit",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#fca311',command=root.destroy)
+exit_btn= Button(c4,text= "Exit",font=myFont1,relief=SOLID,borderwidth=1,height=1,width=12,activebackground='#e5e5e5',bg='#ffb703',command=root.destroy)
 exit_btn.pack(pady=15)
 clubs_btn.bind('<Enter>', on_enter)
 clubs_btn.bind('<Leave>', on_leave)
