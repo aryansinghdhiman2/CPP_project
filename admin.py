0bb377229105fa43fd7b29b901cc2a3b520626f1
Hargun Kaur <hargunkaur625@gmail.com>
2023-01-30 00:30:11 +0530

    .

--- admin.py
+++ admin.py
@@ -305,13 +305,13 @@ def login(root,connection):
 
     user= Entry(frame,width=25,fg="black",border=0,bg="white",font=("Microsoft YaHei UI Light",11))
     user.place(x=30,y=80)
-    user.insert(0,"admin")
+    user.insert(0,"Username")
     
     Frame(frame,width=295,height=2,bg="black").place(x=25,y=107)
 
     code= Entry(frame,width=25,fg="black",border=0,bg="white",font=("Microsoft YaHei UI Light",11))
     code.place(x=30,y=150)
-    code.insert(0,"root")
+    code.insert(0,"password")
     Frame(frame,width=295,height=2,bg="black").place(x=25,y=177)
 
 
